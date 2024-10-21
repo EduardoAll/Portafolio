7cb8e25316f4b4485070498396bfccd2cfbf106f
EduardoAll <104133178+EduardoAll@users.noreply.github.com>
2024-10-21 18:57:55 -0300

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ import {
   AiOutlineFacebook,
 } from "react-icons/ai";
 
-//asdasdasdasdASDASDASDASDasdasdasdasdasdasdasdasdadaasdasdasdasdasdsaasdasdasdasdasdasdasdadasdasdasdasdsaasdasdasddasdsa
+//asdasdasdasdASDASDASDASDasdasdasdasdasdasdasdasdadaasdasdasdasdasdsaasdasdasdasdasdas
 
 const Hero = () => {
   return (
